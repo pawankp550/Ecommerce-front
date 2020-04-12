d3706452c43d126eb03b8e407466317c944c6718
pawankp550 <pawankp550@gmail.com>
2020-04-12 06:37:16 +0000

    moved everything under app.js

--- ecomm-front/src/components/App.js
+++ ecomm-front/src/components/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-
+import Routes from './Routes'
 function App() {
   return (
-    <div className="App">
-      <h1>Ecomm app</h1>
-    </div>
+    <Routes/>
   );
 }
 
--- ecomm-front/src/index.js
+++ ecomm-front/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Routes from './components/Routes';
+import App from './components/App';
 
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
